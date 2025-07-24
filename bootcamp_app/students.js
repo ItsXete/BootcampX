3aed678e877340841d9f6022307a9c1cbde713ea
ItsXete <[dgiabach2000@gmail.com]>
2025-07-23 20:00:51 -0400

    added postgres

---
+++ bootcamp_app/students.js
@@ -0,0 +1,30 @@
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  user: "development",
+  password: "development",
+  host: "localhost",
+  database: "bootcampx",
+});
+
+const cohortName = process.argv[2] || "";  // Get cohort name from CLI arg or default empty string
+const limit = parseInt(process.argv[3], 10) || 5;  // Get limit from CLI arg or default 5
+
+const queryString = `
+  SELECT students.id AS student_id, students.name, cohorts.name AS cohort
+  FROM students
+  JOIN cohorts ON cohorts.id = students.cohort_id
+  WHERE cohorts.name LIKE $1
+  LIMIT $2;
+`;
+
+const queryParams = [`%${cohortName}%`, limit];
+
+pool
+  .query(queryString, queryParams)
+  .then((res) => {
+    res.rows.forEach(user => {
+      console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+    });
+  })
+  .catch((err) => console.error("query error", err.stack));
---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,32 @@
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  user: "development",
+  password: "development",
+  host: "localhost",
+  database: "bootcampx",
+});
+
+const cohortName = process.argv[2] || "";
+
+const queryString = `
+  SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort
+  FROM assistance_requests
+  JOIN teachers ON teachers.id = assistance_requests.teacher_id
+  JOIN students ON students.id = assistance_requests.student_id
+  JOIN cohorts ON cohorts.id = students.cohort_id
+  WHERE cohorts.name = $1
+  ORDER BY teacher;
+`;
+
+const queryParams = [cohortName];
+
+pool
+  .query(queryString, queryParams)
+  .then((res) => {
+    console.log("connected");
+    res.rows.forEach((row) => {
+      console.log(`${row.cohort}: ${row.teacher}`);
+    });
+  })
+  .catch((err) => console.error("query error", err.stack));
